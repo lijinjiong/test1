23611c411d08632aeec53663c1ec4d6417abfe9b
李金炯 <18060915400@163.com>
2017-04-28 18:24:13 +0800

    新增mobile项目

--- backend/views/hospital/index.php
+++ backend/views/hospital/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel common\models\search\HospitalSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Hospitals';
+$this->title = '医院';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="hospital-index">
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Hospital', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('新增医院', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
