cd034ea2c59540685c5b8f766d92f1bff7220884
ljjPC <18060915400>
2017-04-30 18:56:36 +0800

    后台预约管理修改

--- backend/views/reservation/_form.php
+++ backend/views/reservation/_form.php
@@ -17,13 +17,19 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'referrer')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'doc_id')->textInput() ?>
+    <div class="form-group">
+        <label >医生</label>
+        <div class="form-control" ><?=$model->doctor->username?></div>
+    </div>
+   <!-- --><?/*= $form->field($model, 'doc_id')->textInput() */?>
     <?= $form->field($model, 'status')->textInput() ?>
     <?= $form->field($model, 'note')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'add_time')->textInput() ?>
-
+   <!-- --><?/*= $form->field($model, 'add_time')->textInput() */?>
+    <div class="form-group">
+        <label >添加时间</label>
+        <div class="form-control" ><?=Yii::$app->formatter->asDate($model->add_time, 'yyyy-MM-dd H:i:s');?></div>
+    </div>
     <?= $form->field($model, 'reservation_time')->textInput() ?>
 
     <div class="form-group">
